5fa2180cb07fa3f42d75258a96d267e149ff2a3e
abelje <161378812+abelje@users.noreply.github.com>
2025-03-03 14:33:37 -0600

    Test of all materials, weird result

--- metallic.cpp
+++ metallic.cpp
@@ -4,8 +4,14 @@
 #include "hit.h"
 
 Metallic::Metallic(Color color, bool emitting, double fuzz)
-    : Material("Metallic", color, emitting), fuzz(fuzz) {}
+    : Material("Metallic", color, emitting), fuzz(fuzz) {
+    if (fuzz > 1 || fuzz < 0) {
+        throw std::runtime_error("Fuzz value must be in the range [0,1]");
+    }
+}
 
 Ray Metallic::scatter(const Ray& ray, const Hit& hit) const {
-
+    Vector3D reflected = reflect(ray.direction, hit.normal);
+    reflected = unit(reflected) + (fuzz * random_unit_vector());
+    return Ray{hit.position, reflected};
 }
--- test-materials.cpp
+++ test-materials.cpp
@@ -11,6 +11,9 @@
 #include "world.h"
 #include "diffuse.h"
 #include "glass.h"
+#include "lambertian.h"
+#include "specular.h"
+#include "metallic.h"
 
 
 Color trace(const World& world, const Ray& ray);
@@ -21,6 +24,105 @@ void in_class();
 void test_all();
 
 int main() {
+    // in_class();
+
+    // materials
+    Diffuse ground {Gray, false};
+    Diffuse blue{Blue, false};
+    Lambertian lime{Lime, false};
+    Specular purple{Purple, false};
+    Metallic bronze{Orange, false, 0.5};
+    Diffuse light{White, true};
+
+    // world
+    World world;
+    world.add({0, 0, 0.5}, 0.3, &blue);
+    world.add({2, 0.5, 0.5}, 0.3, &lime);
+    world.add({2, -1, 0.5}, 0.3, &purple);
+    world.add({2.75, -0.5, 0.5}, 0.3, &bronze);
+    world.add({0,0, -100}, 100, &ground);
+    world.add({0, 0, 0}, 100, &light);
+
+    // specify the number of pixels
+    Pixels pixels{1280, 720};
+
+    // Set up camera
+    Vector3D position{10, -2, 3}, up{0, 0, 1};
+    Vector3D target{0, 0, 0};
+    double fov{20};
+    double aspect = static_cast<double>(pixels.columns) / pixels.rows;
+    Camera camera{position, target, up, fov, aspect};
+
+    constexpr int samples = 10000;
+    constexpr int ray_depth = 5;
+
+    const long long rays_total = pixels.rows* pixels.columns * static_cast<long long>(samples);
+    long long ray_num = 0;
+
+    for (auto row = 0; row < pixels.rows; ++row) {
+        for (auto col = 0; col < pixels.columns; ++col) {
+            for (int N = 0; N < samples; ++N) {
+                double y = (row + random_double()) / (pixels.rows - 1);
+                double x = (col + random_double()) / (pixels.columns - 1);
+                // cast samples number of rays
+                Ray ray = camera.compute_ray(x, y);
+                pixels(row, col) += trace_path(world, ray, ray_depth);
+
+                ++ray_num;
+                if (ray_num % (rays_total / 100) == 0) {
+                    print_progress(ray_num, rays_total);
+                }
+            }
+            pixels(row, col) /= samples;
+        }
+    }
+
+    std::string filename{"spheres-test.png"};
+    pixels.save_png(filename);
+    std::cout << "Wrote " << filename << '\n';
+}
+
+Color trace(const World& world, const Ray& ray) {
+    std::optional<Hit> hit = world.find_nearest(ray);
+    if (hit.has_value()) {
+        return hit->normal;
+    }
+    else {
+        return Black;
+    }
+}
+
+Color trace_path(const World& world, const Ray& ray, int depth) {
+    if (depth == 0) {
+        return Black;
+    }
+
+    std::optional<Hit> hit = world.find_nearest(ray);
+    if (!hit) {
+        return Blue; //out of color palatte, so its an obvious edge case
+        // artifacts?
+    }
+
+    const Material* material = hit->sphere->material;
+    if (material->emitting) {
+        return material->color;
+    }
+
+    //more bounces!
+    Ray scattered = material->scatter(ray, hit.value());
+    return material->color * trace_path(world, scattered, depth-1);
+}
+
+void print_progress(long long ray_num, long long total_rays) {
+    auto width = std::to_string(total_rays).length() + 4;
+    int percentage = std::round(static_cast<double>(ray_num) / total_rays * 100);
+
+    std::cout << "\rProgram:" << std::setw(3) << percentage << "%";
+    std::cout << std::setw(width) << ray_num << '/' << total_rays << " rays";
+    std::cout << std::flush;
+}
+
+void in_class() {
     // materials
     Diffuse red{{1, 0, 0}, false};
     Diffuse gray{{0.5, 0.5, 0.5}, false};
@@ -73,48 +175,4 @@ int main() {
     std::string filename{"sphere1.png"};
     pixels.save_png(filename);
     std::cout << "Wrote " << filename << '\n';
-}
-
-Color trace(const World& world, const Ray& ray) {
-    std::optional<Hit> hit = world.find_nearest(ray);
-    if (hit.has_value()) {
-        return hit->normal;
-    }
-    else {
-        return Black;
-    }
-}
-
-Color trace_path(const World& world, const Ray& ray, int depth) {
-    if (depth == 0) {
-        return Black;
-    }
-
-    std::optional<Hit> hit = world.find_nearest(ray);
-    if (!hit) {
-        return Blue; //out of color palatte, so its an obvious edge case
-        // artifacts?
-    }
-
-    const Material* material = hit->sphere->material;
-    if (material->emitting) {
-        return material->color;
-    }
-
-    //more bounces!
-    Ray scattered = material->scatter(ray, hit.value());
-    return material->color * trace_path(world, scattered, depth-1);
-}
-
-void print_progress(long long ray_num, long long total_rays) {
-    auto width = std::to_string(total_rays).length() + 4;
-    int percentage = std::round(static_cast<double>(ray_num) / total_rays * 100);
-
-    std::cout << "\rProgram:" << std::setw(3) << percentage << "%";
-    std::cout << std::setw(width) << ray_num << '/' << total_rays << " rays";
-    std::cout << std::flush;
-}
-
-void in_class() {
-
 }
