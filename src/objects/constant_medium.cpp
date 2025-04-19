cd531112d6d091a72abcd00e6424ede82da69383
abelje <161378812+abelje@users.noreply.github.com>
2025-04-18 20:47:06 -0500

    In Class Work for Fog.

--- src/objects/constant_medium.cpp
+++ src/objects/constant_medium.cpp
@@ -1,6 +1,20 @@
 #include "constant_medium.h"
 
+#include "constants.h"
+#include "hit.h"
+#include "ray.h"
+
 ConstantMedium::ConstantMedium(Object *boundary, double density, const Material* material)
     :Object{material}, boundary{boundary}, density{density} {}
 
+std::optional<double> ConstantMedium::intersect(const Ray& ray) const {
+    auto t1 = boundary->intersect(ray);
+    if (!t1) {
+        return {};
+    }
+
+    Hit hit = boundary->construct_hit(ray, t1.value);
+    Ray ray1{ray.at(t1.value() + 2*Constants::epsilon), ray.direction};
+    auto t2 = boundary->intersect(ray1);
+}
 
--- src/objects/constant_medium.h
+++ src/objects/constant_medium.h
@@ -6,6 +6,11 @@ class ConstantMedium : public Object {
 public:
     ConstantMedium(Object* boundary, double density, const Material* material);
 
+    virtual AABB bounding_box() const = 0;
+    virtual std::optional<double> intersect(const Ray& ray) const override;
+    virtual Hit construct_hit(const Ray& ray, double time) const override;
+    virtual std::pair<double, double> uv(const Hit& hit) const override;
+
     Object* boundary;
     double density;
 };
